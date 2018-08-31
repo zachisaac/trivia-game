9e49e1747a8eb2866cba0dc2eba20f4b34d2722f
zpaki5142 <zachisaac@users.noreply.github.com>
2018-08-30 17:24:01 -0700

    First Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+//Javascript Car and Racing Trivia
+
+//Which car manufactuerer was supposed to be sold to Ford in 1962 for 12m dollars?
+
+//The Model T was only available in which color?
+
+//Which Manufacteurer started out making tractors?
+
+//Which car Manufacteurer started out working for VW?
+
+//Which car Manufacteurer has more 24 LeMans victories than any other?
+
+//Which driver has the most Formula 1 World Championships?
+
+//Which NASCAR driver has the most championships?
+
+//The most expensive car ever sold at auction was?
+
+//The longest race track in the world is call?
+
+//The Ford GT40 get its name because it is...?
+
+//Which Manufacteurer has the most Formula 1 World Championships?
+
+//Bruce McLaren is from which country?
+
+//The Bugatti Veyron has how many turbos?
+
+//A NHRA Top Fuel drag car produces how much horsepower?
+
+//What is the first race of the NASCAR season?
+
+//Porsche started in which German city?
+
+//What is Mercedes performance division?
+
+//What is BMW's performance division?
+
+//The Pike Peak Hill Climb takes place in what state?
+
+//The 24 Hour of LeMans takes place in which country?
